8dfc8b7258e05a699f47d443f0754d8ed22ec439
finkelstein <finkelstein@adelphi.edu>
2018-10-24 08:20:33 -0400

    Set up UI Widgets in activity.

--- app/src/main/java/example/com/takeapic/MainActivity.java
+++ app/src/main/java/example/com/takeapic/MainActivity.java
@@ -1,13 +1,21 @@
 package example.com.takeapic;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mButton;
+    private ImageView mImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = (Button)findViewById(R.id.button);
+        mImageView = (ImageView)findViewById(R.id.image);
     }
 }
